159c09eed5dbdfdd9442087fe05cc55369637006
munraj <munrajgrewal12@gmail.com>
2021-02-08 23:06:29 +0000

    added all queries except most liked

--- routes/stories.js
+++ routes/stories.js
@@ -8,6 +8,40 @@ module.exports = (db) => {
   //have the loaded either completed or wip stories below
   router.get("/", (req, res) => {
     res.render("completed and wip stories page")
+
+    // sort by completed
+    if ("sort by complete") {
+      // sort by order added
+      if ("sort by completed and id ascending") {
+        db.query('SELECT * FROM stories WHERE is_complete = true ORDER BY id ASC;');
+      } else if ("sort by completed and id decending") {
+        db.query('SELECT * FROM stories WHERE is_complete = true ORDER BY id DESC;');
+      } else {
+        db.query('SELECT * FROM stories WHERE is_complete = true;');
+      }
+    // sort by incomplete
+    } else if ("sort by incomplete") {
+      // sort by order added
+      if ("sort by incomplete and id ascending") {
+        db.query('SELECT * FROM stories WHERE is_complete = false ORDER BY id ASC;');
+      } else if ("sort by incomplete and id decending") {
+        db.query('SELECT * FROM stories WHERE is_complete = false ORDER BY id DESC;');
+      } else {
+        db.query('SELECT * FROM stories WHERE is_complete = false;');
+      }
+    // search by user
+    } else if("user wants to search by specific user") {
+      db.query('SELECT * FROM stories WHERE cretor_id = _____;');
+    // search by keywords in title
+    } else if("user wants to search titles by keyword/phrases") {
+      db.query(`SELECT * FROM stories WHERE story_title LIKE '%_____%';`);
+    // sort by most liked
+    } else if("sort by most liked") {
+
+    }else {
+      db.query('SELECT * FROM stories;');
+    }
+
   });
 
   //redirects to /view/:id to display specific story/wip
@@ -27,6 +61,7 @@ module.exports = (db) => {
   router.get("/:id", (req, res) => {
     //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.userId]};
     res.render("show story page, templatevars")
+    //all current users stories
   });
 
 
