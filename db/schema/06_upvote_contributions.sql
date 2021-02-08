0f5cff57f5ab3905e47d8f20f3e3e017d99f2720
munraj <munrajgrewal12@gmail.com>
2021-02-08 19:22:28 +0000

    fixed upvote contribution table

--- db/schema/06_upvote_contributions.sql
+++ db/schema/06_upvote_contributions.sql
@@ -3,7 +3,6 @@
 DROP TABLE IF EXISTS upvote_contribution CASCADE;
 CREATE TABLE upvote_contribution (
   id SERIAL PRIMARY KEY NOT NULL,
-  -- change story_id to user_id in erd --
   user_id INTEGER REFERENCES users(id),
   contribution_id INTEGER REFERENCES contributions(id)
 );
