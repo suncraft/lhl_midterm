df13b85673edd140b76d28b99964b197fbc4c1fb
munraj <munrajgrewal12@gmail.com>
2021-02-11 04:49:33 +0000

    main page form post to database insert is working

--- routes/view.js
+++ routes/view.js
@@ -18,83 +18,15 @@ module.exports = (db) => {
   //add a contribution you wrote
   //upvote a contribution
   router.post("/:id", function(req, res) {
-    //if complete status was toggled, change db, re-render page with jquery
-    if (false) {
-
-      //this probably isnt correct syntax mainly here to remind of what to do -Munraj
-      let isComplete = db.query('SELECT is_complete FROM stories WHERE id = _____;');
-      if(isComplete){
-        db.query(`UPDATE stories SET is_complete = false WHERE id = ________;`)
-        .then(data => {
-          const users = data.rows;
-          res.json({ users });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-      } else {
-        db.query(`UPDATE stories SET is_complete = true WHERE id = ________;`)
-        .then(data => {
-          const users = data.rows;
-          res.json({ users });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-      }
-
-      return;
-    };
-
-    //if contribution is accepted, add to accepted_story_contributions, re-render story
-    if (false) {
-      //add update query
-      db.query(`INSERT INTO accepted_story_contributions (story_id, contribution_id) VALUES (_____, _____);`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-      return;
-    }
-
-    //if add contribution button clicked, add to db, re-render
-    if (false) {
-      db.query(`INSERT INTO contributions (story_id, contribution, user_id) VALUES (_____, '_____', _____);`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-      return res.render("jquery refresh how does this work again?");
-    }
-
-    //if contribution upvoted, add the upvote to db, re-render
-    if (false) {
-      db.query(`INSERT INTO upvote_contribution (user_id, contribution_id) VALUES (_____, _____);`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-      return;
-    }
+    db.query(`INSERT INTO stories (cretor_id, story_title, story_beginning, is_complete) VALUES (${req.session.userId}, '${req.body.title}', '${req.body.story}', false);`)
+    .then(data => {
+      console.log("query executed")
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
   });
   return router;
 };
