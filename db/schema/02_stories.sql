4b597dde82f98dc665105f76a978827f5e6019f9
Suncraft <cookies666@msn.com>
2021-02-11 23:29:44 +0000

    adjusted database for longer test stories and 2 test pictures

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -5,7 +5,7 @@ CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   cretor_id INTEGER REFERENCES users(id),
   story_title VARCHAR(255) NOT NULL,
-  story_beginning VARCHAR(255) NOT NULL,
+  story_beginning VARCHAR NOT NULL,
   is_complete BOOLEAN NOT NULL,
   cover_photo_url VARCHAR(255)
 );
--- db/schema/03_contributions.sql
+++ db/schema/03_contributions.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS contributions CASCADE;
 CREATE TABLE contributions (
   id SERIAL PRIMARY KEY NOT NULL,
   story_id INTEGER REFERENCES stories(id),
-  contribution VARCHAR(255) NOT NULL,
+  contribution VARCHAR NOT NULL,
   user_id INTEGER REFERENCES users(id)
 );
