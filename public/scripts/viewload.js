a9f97d12d7e3bd3f09f00fbc832fc684e212b17a
munraj <munrajgrewal12@gmail.com>
2021-02-12 08:11:10 +0000

    fixed some id handling

--- public/scripts/viewload.js
+++ public/scripts/viewload.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
       let $string = $(this).serialize();
       let storyId = $("article.full").attr("id")
       $string += `&storyId=${storyId}`;
+      console.log($string)
       //errors are not currently working, were before I changed post to get?
       if ($(this.children[2]).length < 1) {
         throw new Error("Please enter text for contributing to a story.");
